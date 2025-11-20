17f8033e64d4bd9b2ee2357f47b4af9a4ee40d76
PascalNehlsen <pascaldev92@gmail.com>
2025-11-20 20:42:09 +0100

    feat: add lint action

--- babyshop_app/users/views.py
+++ babyshop_app/users/views.py
@@ -1,40 +1,44 @@
 from django.shortcuts import redirect, render
-from . forms import LoginForm, RegisterForm
-from django.contrib.auth import authenticate,login,logout
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+
 def user_register(request):
-    if request.method=="POST":
-        form=RegisterForm(request.POST)
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request,'Your account has been created, you can log in')
-            messages.success(request,'Your account has been created, you can log in')
-            return redirect('login')
+            messages.success(request, "Your account has been created, you can log in")
+            messages.success(request, "Your account has been created, you can log in")
+            return redirect("login")
     else:
-        form=RegisterForm()
+        form = RegisterForm()
 
-    return render(request,'register.html',{'form':form})
+    return render(request, "register.html", {"form": form})
+
+
 def user_login(request):
-    if request.method=="POST":
-        form=LoginForm(request.POST)
+    if request.method == "POST":
+        form = LoginForm(request.POST)
         if form.is_valid():
-            username=form.cleaned_data['username']
-            password=form.cleaned_data['password']
-            user= authenticate(request,username=username,password=password)
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 if user.is_active:
-                    login(request,user)
-                    return redirect('/')
+                    login(request, user)
+                    return redirect("/")
                 else:
-                    messages.info(request,'User not active')
+                    messages.info(request, "User not active")
             else:
-                messages.info(request,'Please check your login details')
+                messages.info(request, "Please check your login details")
 
     else:
-        form=LoginForm()
-    return render(request,'login.html',{'form':form})
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
+
 
 def user_logout(request):
     logout(request)
-    return redirect('/')
+    return redirect("/")
